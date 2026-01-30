83a12877c3c015c20702185e31467bc51ca44da9
Throne-of-Doom <cg-86@live.com>
2026-01-30 00:03:29 -0500

    Implement thumbnail upload handler with authorization checks

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,48 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	data, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid file upload", err)
+		return
+	}
+
+	defer data.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+
+	image, err := io.ReadAll(data)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to read file", err)
+		return
+	}
+
+	metaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+	if userID != metaData.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", nil)
+		return
+	}
+
+	imageData := base64.StdEncoding.EncodeToString(image)
+
+	dataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, imageData)
+
+	metaData.ThumbnailURL = &dataURL
+
+	err = cfg.db.UpdateVideo(metaData)
+	if err != nil {
+		respondWithError(w, 500, "error updating video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, metaData)
 }
