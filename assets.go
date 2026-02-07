2ec27b23eabc1f930bb9aba4c6b736e5a2fda1eb
User <user@example.com>
2026-02-06 19:32:09 -0500

    Fix aspect ratio prefix assignment for video uploads

--- assets.go
+++ assets.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
+
+type ffprobeOutput struct {
+	Streams []struct {
+		Width  int `json:"width"`
+		Height int `json:"height"`
+	} `json:"streams"`
+}
 
 func (cfg apiConfig) ensureAssetsDir() error {
 	if _, err := os.Stat(cfg.assetsRoot); os.IsNotExist(err) {
@@ -44,4 +55,32 @@ func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
 
 func (cfg apiConfig) getAssetURL(assetPath string) string {
 	return "http://localhost:" + cfg.port + assetPath
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	var output ffprobeOutput
+	if err := json.Unmarshal(out.Bytes(), &output); err != nil {
+		return "", err
+	}
+	if len(output.Streams) == 0 {
+		return "", fmt.Errorf("no video streams found")
+	}
+	width := output.Streams[0].Width
+	height := output.Streams[0].Height
+
+	if width > height {
+		return "16:9", nil
+	}
+	if height > width {
+		return "9:16", nil
+	}
+	return "other", nil
 }
--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,136 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	const uploadLimit = 1 << 30
+
+	r.Body = http.MaxBytesReader(w, r.Body, uploadLimit)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	metaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+	if userID != metaData.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", nil)
+		return
+	}
+	videoFile, videoHeader, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "could not parse video file", err)
+		return
+	}
+
+	defer videoFile.Close()
+
+	contentType := videoHeader.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType((contentType))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid file type", err)
+		return
+	}
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "invalid file type", nil)
+		return
+	}
+
+	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error creating file", nil)
+		return
+	}
+
+	defer tempFile.Close()
+	defer os.Remove(tempFile.Name())
+
+	_, err = io.Copy(tempFile, videoFile)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to copy file", err)
+		return
+	}
+
+	_, err = tempFile.Seek(0, io.SeekStart)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to seek file", err)
+		return
+	}
+
+	aspectRatio, err := getVideoAspectRatio(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error getting aspect ratio", err)
+		return
+	}
+	prefix := "other"
+	if aspectRatio == "16:9" {
+		prefix = "landscape"
+	}
+	if aspectRatio == "9:16" {
+		prefix = "portrait"
+	}
+
+	s3Key := make([]byte, 32)
+
+	_, err = rand.Read(s3Key)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error creating filename", err)
+		return
+	}
+
+	key := hex.EncodeToString(s3Key) + ".mp4"
+	key = path.Join(prefix, key)
+
+	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:      aws.String(cfg.s3Bucket),
+		Key:         aws.String(key),
+		Body:        tempFile,
+		ContentType: aws.String(mediaType),
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error uploading file to s3", err)
+		return
+	}
+
+	url := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
+	metaData.VideoURL = &url
+
+	err = cfg.db.UpdateVideo(metaData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "could not update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, metaData)
+}
